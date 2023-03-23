0e371d4184f47fcafa59ec85be6c66794c90c98f
atrconic <ana.trconic@gmail.com>
2023-03-23 19:41:21 +0100

    added example ajax that sends dummydata to flask

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
 
 
 @app.route("/")
 def render_index():
     return render_template("view.html")
+
+
+@app.route("/action", methods=['POST'])
+def do_action():
+    d = {
+        "a": "b",
+    }
+    return jsonify(d)
 
 
 
--- static/app.js
+++ static/app.js
@@ -1,4 +1,17 @@
 function doSend(color) {
-    alert("Hello " + color)
+    console.log("Hello " + color);
+    $.ajax({
+        type: "POST",
+        url: "/action",
+        data: '{"color": "blue"}',
+        success: (msg) => {
+            console.log("my success", msg)
+        },
+        error: failFunction,
+        dataType: "json"
+    });
+}
+function failFunction(xhr, errmsg) {
+    console.log("my error", errmsg)
 }
 
