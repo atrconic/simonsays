3c40c8bf2ab27f583c133a370b475340f8ba3450
atrconic <ana.trconic@gmail.com>
2023-03-25 22:54:23 +0100

    added a color that appears and disappears

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
@@ -7,10 +8,19 @@ colors = ["blue", "red", "green", "yellow"]
 guess = []
 actions = []
 
+
 @app.route("/")
 def render_index():
     reset()
     return render_template("view.html")
+
+
+@app.route("/get-guess")
+def get_guess():
+    global guess
+    #time.sleep(1)
+    print("boja", guess[-1])
+    return jsonify(guess[-1])
 
 
 @app.route("/action", methods=['POST'])
@@ -18,18 +28,23 @@ def do_action():
     global actions, guess
     data = request.json
     color_var = data.get("color")
+    is_end = False if color_var == guess[-1] else True
     actions.append(color_var)
+    guess.append(rand_color())
     res = jsonify({
         'actions': actions,
         'guess': guess,
+        'is_end': is_end,
     })
-    guess.append(rand_color())
+    if is_end:
+        reset()
     return res
 
 
 def reset():
     global actions, guess
     guess = [rand_color()]
+    print(guess)
     actions = []
 
 
--- static/app.js
+++ static/app.js
@@ -7,13 +7,70 @@ function doSend(colorVar) {
         data:  JSON.stringify({color: colorVar}),
         // data: '{"color": "blue"}',
         success: (msg) => {
-            console.log("my success", msg)
+            console.log("my success", msg);
+            getGuess();
         },
         error: failFunction,
         dataType: "json"
     });
 }
+
 function failFunction(xhr, errmsg) {
-    console.log("my error", errmsg)
+    console.log("my error", errmsg);
 }
+
+function sakrijGuess() {
+    setTimeout(function(){
+        let guessDiv = $("#guess-div");
+        guessDiv.removeClass("bg-primary");
+        guessDiv.removeClass("bg-danger");
+        guessDiv.removeClass("bg-success");
+        guessDiv.removeClass("bg-warning");
+        enabledButtons();
+    }, 1000);
+}
+
+function getGuess() {
+    disabledButtons();
+    $.ajax({
+        type: "GET",
+        url: "/get-guess",
+        success: (msg) => {
+            console.log("guess", msg);
+            showGuess(msg);
+            sakrijGuess(msg);
+        },
+        error: (err, xhr) => {
+            console.log("guess error", err);
+        },
+        dataType: "json"
+    });
+    //console.log(1);
+}
+
+function showGuess(color) {
+    let guessDiv = $("#guess-div");
+    if (color === "blue") {
+        guessDiv.addClass("bg-primary");
+    } else if (color === "red") {
+        guessDiv.addClass("bg-danger");
+    } else if (color === "green") {
+        guessDiv.addClass("bg-success");
+    } else if (color === "yellow") {
+        guessDiv.addClass("bg-warning");
+    }
+}
+
+function disabledButtons() {
+    $("button").prop('disabled', true);
+}
+
+function enabledButtons() {
+    $("button").prop('disabled', false);
+}
+
+$(document).ready(function(){
+console.log("pocetak")
+    getGuess();
+});
 
